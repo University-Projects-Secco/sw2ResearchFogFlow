bc9392935c6952c17db32a5ee123898b09921a6c
Galaxar <matteo.secco97@gmail.com>
2020-06-07 16:17:08 +0200

    Update idle notifier -Finished function

--- smart_factory/docker images/idle_notifier/src/main/java/com/fogflow/fogfunction/FogFunction.java
+++ smart_factory/docker images/idle_notifier/src/main/java/com/fogflow/fogfunction/FogFunction.java
@@ -13,8 +13,23 @@ public class FogFunction {
     private static final String LAST_RESULT_ATTRIBUTE = "last_interval";
     public static void function(@NotNull ContextObject entity, @NotNull RestHandler restHandler) {
         final String originalEntityId = entity.id.replace(RESULT_ID_PREFIX,"");
-        if(((String)entity.attributes.get(STATUS_ATTRIBUTE).value).equalsIgnoreCase("idle") && ((int)entity.attributes.get(LAST_RESULT_ATTRIBUTE).value)>0){
+        if(((String)entity.attributes.get(STATUS_ATTRIBUTE).value).equalsIgnoreCase("idle") && ((int)entity.attributes.get(LAST_RESULT_ATTRIBUTE).value)>10){
             publishLog("IDLE ERROR: "+entity.id.replace(RESULT_ID_PREFIX,""),restHandler);
+            ContextObject error = new ContextObject();
+            error.id = "ERROR: "+originalEntityId;
+            error.type = "Error";
+            ContextAttribute description = new ContextAttribute();
+            description.name = "error";
+            description.type = "string";
+            description.value = "Entity "+originalEntityId+" was idle for too long";
+            ContextAttribute interval = new ContextAttribute();
+            interval.name = "time_passed";
+            interval.type = "number";
+            interval.value = entity.attributes.get(LAST_RESULT_ATTRIBUTE);
+            ContextMetadata time = new ContextMetadata();
+            time.name = "time";
+            time.type = "string";
+            time.value = new Date().toString();
         }
     }
 
