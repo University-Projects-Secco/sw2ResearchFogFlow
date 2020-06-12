d491796ad35d400ca3ad79820a111e1fbb9197f4
Galaxar <matteo.secco97@gmail.com>
2020-06-12 13:12:32 +0200

    Update build_docker.sh

--- smart_factory/docker_images/calculator/build.sh
+++
@@ -1,5 +0,0 @@
-mvn clean install
-#replace the tag if you want to modify the fog function
-docker build -t "galaxarum/detect_idle" .
-#the modified version must be uploaded to docker hub
-docker push galaxarum/detect_idle
