e9ebab2e07ae528122659b2a7219914a405f0d78
Galaxar <matteo.secco97@gmail.com>
2020-06-12 13:12:32 +0200

    Update build_docker.sh

--- smart_factory/sh_scripts/build_docker.sh
+++ smart_factory/sh_scripts/build_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 for file in $(find ../docker_images -regex ".*/build.*sh$")
 do
+  printf '\n\n'
   echo '---------------'
   echo "$file"
   echo '---------------'
+  printf '\n\n'
   sh "$file"
 done
 wait
