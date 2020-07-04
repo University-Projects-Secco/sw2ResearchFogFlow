daaba369a46bf37c6f7967b76800a91f33ee7e17
Galaxar <matteo.secco97@gmail.com>
2020-07-04 17:50:06 +0200

    Update status fog function

--- smart_factory/docker_images/state_detector/src/main/java/com/fogflow/fogfunction/FogFunction.java
+++ smart_factory/docker_images/state_detector/src/main/java/com/fogflow/fogfunction/FogFunction.java
@@ -18,7 +18,7 @@ public class FogFunction {
                 Collections.emptyList());
 
         if(queryResults.size()>1){
-            publishLog("Too many results for one device!",restHandler);
+            restHandler.publishLog("Too many results for one device!");
             return;
         }
         final ContextObject newResult = new ContextObject();
@@ -36,33 +36,18 @@ public class FogFunction {
         attributes.get(LAST_RESULT_ATTRIBUTE).value = 0;
         if(!queryResults.isEmpty() ){
             final ContextObject lastResult = new ContextObject(queryResults.get(0));
-            publishLog("last status: "+lastResult.attributes.get(STATUS_ATTRIBUTE).value,restHandler);
-            publishLog("new status: "+entity.attributes.get(STATUS_ATTRIBUTE).value,restHandler);
-            publishLog("Evaluating 'if': old status: "+lastResult.attributes.get(STATUS_ATTRIBUTE).value+" new status: "+attributes.get(STATUS_ATTRIBUTE).value+
-                    "expected new interval: "+( ( long ) lastResult.attributes.get(LAST_RESULT_ATTRIBUTE).value + //new interval = old interval + new time - old time
-                                            ( long ) attributes.get(TIME_ATTRIBUTE).value -
-                                            ( long ) lastResult.attributes.get(TIME_ATTRIBUTE).value),restHandler);
             if(lastResult.attributes.get(STATUS_ATTRIBUTE).value.equals(entity.attributes.get(STATUS_ATTRIBUTE).value)) {
-                attributes.get(LAST_RESULT_ATTRIBUTE).value =
-                        ( long ) lastResult.attributes.get(LAST_RESULT_ATTRIBUTE).value + //new interval = old interval + new time - old time
-                        ( long ) attributes.get(TIME_ATTRIBUTE).value -
-                        ( long ) lastResult.attributes.get(TIME_ATTRIBUTE).value;
-                publishLog("Update time\nNew value: "+ attributes.get(LAST_RESULT_ATTRIBUTE).value,restHandler);
+                final long newInterval = (Long.parseLong(lastResult.attributes.get(LAST_RESULT_ATTRIBUTE).value.toString()) + //new interval = old interval + new time - old time
+                        Long.parseLong(attributes.get(TIME_ATTRIBUTE).value.toString()) -
+                        Long.parseLong(lastResult.attributes.get(TIME_ATTRIBUTE).value.toString()));
+                restHandler.publishLog("Last result expected value:" + newInterval);
+                attributes.get(LAST_RESULT_ATTRIBUTE).value = newInterval;
+                restHandler.publishLog("Update time, New value: "+ attributes.get(LAST_RESULT_ATTRIBUTE).value);
             }
         }
         newResult.attributes = attributes;
         restHandler.publishResult(newResult,false);
     }
 
-    private static void publishLog(@NotNull String value, @NotNull RestHandler restHandler) {
-        ContextObject resultEntity = new ContextObject();
-        resultEntity.id = "Log "+new Date().toString();
-        resultEntity.type = "Log";
-        ContextAttribute attr = new ContextAttribute();
-        attr.name = "text";
-        attr.type = "string";
-        attr.value = value;
-        resultEntity.attributes.put("text", attr);
-        restHandler.publishResult(resultEntity, false);
-    }
+    
 }
