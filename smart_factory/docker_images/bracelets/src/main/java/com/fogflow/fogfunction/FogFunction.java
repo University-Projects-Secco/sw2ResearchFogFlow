3f8093d645b62af6cec10a871d4e3bb39b8da9ed
Galaxar <matteo.secco97@gmail.com>
2020-07-05 18:04:09 +0200

    Update bracelets: finalized

--- smart_factory/docker_images/bracelets/src/main/java/com/fogflow/fogfunction/FogFunction.java
+++ smart_factory/docker_images/bracelets/src/main/java/com/fogflow/fogfunction/FogFunction.java
@@ -1,30 +1,51 @@
 package com.fogflow.fogfunction;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class FogFunction {
     private static final String RESULT_ID_PREFIX = "Result.statusTime.";
-    private static final String RESULT_TYPE = "Result";
-    private static final String TIME_ATTRIBUTE = "time";
-    private static final String STATUS_ATTRIBUTE = "status";
-    private static final String LAST_RESULT_ATTRIBUTE = "last_interval";
     private static final String ROBOT_TYPE = "Robot";
     private static final double MIN_DISTANCE = 15;
     private static final String POSITION_ATTRIBUTE = "position";
 
     public static void function(@NotNull ContextObject entity, @NotNull RestHandler restHandler) {
-        final String originalEntityId = entity.id.replace(RESULT_ID_PREFIX,"");
-        if(((String)entity.attributes.get(STATUS_ATTRIBUTE).value).equalsIgnoreCase("idle") && ((int)entity.attributes.get(LAST_RESULT_ATTRIBUTE).value)>0){
-            restHandler.publishLog("IDLE ERROR: "+entity.id.replace(RESULT_ID_PREFIX,""));
-            final List<ContextObject> robots = restHandler.queryContext(Collections.singletonList(
-                    new EntityId(null,ROBOT_TYPE,true)
-            ),Collections.emptyList()).stream().map(ContextObject::new).collect(Collectors.toList());
-            robots.stream()
-                    .map(robot->"position: "+robot.attributes.get(POSITION_ATTRIBUTE).value.getClass().getName())
-                    .forEach(restHandler::publishLog);
+        final List<ContextObject> robots = restHandler.queryContext(Collections.singletonList(
+                new EntityId(null,ROBOT_TYPE,true)
+        ),Collections.emptyList()).stream().map(ContextObject::new).collect(Collectors.toList());
+        restHandler.publishLog("#Robots: "+robots.size());
+        robots.stream()
+                .filter(robot->distance(entity,robot,restHandler)<MIN_DISTANCE)
+                .forEach(robot->restHandler.publishLog(
+                        "Error: distance not respected. Robot ID: "+robot.id+
+                                ", robot position: "+robot.attributes.get(POSITION_ATTRIBUTE).value+
+                                ", human ID: "+entity.id+
+                                ", human position: "+entity.attributes.get(POSITION_ATTRIBUTE).value
+                ));
+    }
+
+    private static double distance(ContextObject bracelet, ContextObject robot, RestHandler restHandler){
+        try {
+            final JsonParser parser = JsonParserFactory.getJsonParser();
+            final Map<String, Object> bracPosition = parser.parseMap(correctJsonFormat(bracelet.attributes.get(POSITION_ATTRIBUTE).value.toString()));
+            final Map<String, Object> robotPosition = parser.parseMap(correctJsonFormat(robot.attributes.get(POSITION_ATTRIBUTE).value.toString()));
+            final double x1 = ( double ) bracPosition.get("latitude");
+            final double x2 = ( double ) robotPosition.get("latitude");
+            final double y1 = ( double ) bracPosition.get("longitude");
+            final double y2 = ( double ) robotPosition.get("longitude");
+            return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
+        }catch ( Exception e ){
+            restHandler.publishLog("Error: "+e.getMessage());
+            return 0;
         }
+    }
+
+    private static @NotNull String correctJsonFormat(@NotNull String in){
+        return in.replaceAll("="," : ")
+                .replaceAll("([a-zA-Z]+)","\"$0\" ");
     }
 }
