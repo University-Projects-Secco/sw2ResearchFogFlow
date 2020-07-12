a5cd9c07ed3231d62c03c5e1349cb0df88158d19
Galaxar <matteo.secco97@gmail.com>
2020-07-12 12:06:46 +0200

    Moded/renamed sh scripts

--- smart_factory/sh_scripts/all_scripts_register.sh
+++ smart_factory/sh_scripts/all_scripts_register.sh
@@ -4,7 +4,7 @@ sh ./generic_register_operator_no_port.sh measure_robot_status_time "$IP"
 sh ./generic_register_operator_no_port.sh detect_idle "$IP"
 sh ./generic_register_operator_no_port.sh bracelets "$IP"
 wait
-sh ./generic_register_function.sh measure_robot_status_time Robot "$IP" &
-sh ./generic_register_function.sh detect_idle Result "$IP" &
-sh ./generic_register_function.sh bracelets Bracelet "$IP" &
+sh ./generic_register_function.sh measure_robot_status_time Robot "$IP"
+sh ./generic_register_function.sh detect_idle Result "$IP"
+sh ./generic_register_function.sh bracelets Bracelet "$IP"
 wait
