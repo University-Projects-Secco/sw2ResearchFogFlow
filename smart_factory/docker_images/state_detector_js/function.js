da09898edea386f4d3740685b14cf199f95ace87
Galaxar <matteo.secco97@gmail.com>
2020-08-24 15:50:09 +0200

    fixed query in state detector

--- smart_factory/docker_images/state_detector_js/function.js
+++ smart_factory/docker_images/state_detector_js/function.js
@@ -1,34 +1,39 @@
-const RANDOM_CONSTANT = 0
-
 exports.handler = function(contextEntity, publish, query, subscribe,log)
 {
 
-	log("enter state detector");
+    log("enter state detector");
 
-	const id  = "Result.statusTime"+contextEntity.entityId.id
+    const id  = "Result.statusTime"+contextEntity.entityId.id
 
-	query([{id:id, isPattern: false}],
+    query({
+            entities: [
+                {
+                    id:id,
+                    isPattern: false
+                }
+            ]
+        },
         function (entityList){
-	    log('measure status: enter query callback')
-	        if(entityList.length>1){
+            log('measure status: enter query callback. got '+entityList.length+' results')
+            if(entityList.length>1){
                 log('Too many results for one entity!')
                 return
             }
 
-	        const newResult = {
-	            entityId: {
-	                id: id,
+            const newResult = {
+                entityId: {
+                    id: id,
                     type: "Result",
                     isPattern: false
                 },
                 attributes: {
-	                time: {
-	                    type: 'number',
+                    time: {
+                        type: 'number',
                         value: new Date().getTime()
                     },
                     status: contextEntity.attributes.status,
                     last_interval:{
-	                    type: 'number',
+                        type: 'number',
                         value: 0
                     }
                 }
@@ -36,16 +41,16 @@ exports.handler = function(contextEntity, publish, query, subscribe,log)
 
             log('measure status: created response')
 
-	        if (entityList.length===1){
-	            const prevResult = entityList[0]
+            if (entityList.length===1){
+                const prevResult = entityList[0]
                 newResult.attributes.last_interval.value =
                     prevResult.attributes.last_interval.value +
                     newResult.attributes.time.value -
                     prevResult.attributes.time.value
             }
 
-	        log('measure status: publishing result')
-	        publish(newResult)
+            log('measure status: publishing result')
+            publish(newResult)
         })
 };
 
