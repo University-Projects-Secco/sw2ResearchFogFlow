158a0358c124dc63d6bc51147d9050338651fe33
Galaxar <matteo.secco97@gmail.com>
2020-08-24 15:50:23 +0200

    fixed build.sh in state detector

--- smart_factory/docker_images/state_detector_js/build.sh
+++ smart_factory/docker_images/state_detector_js/build.sh
@@ -1,3 +1,3 @@
 npm install  
-docker build -t "galaxarum/detect_idle_js"  .
-docker push galaxarum/detect_idle_js
+docker build -t "galaxarum/measure_robot_status_time_js"  .
+docker push galaxarum/measure_robot_status_time_js
