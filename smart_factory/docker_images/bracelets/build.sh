9a86e131ebcecd4e605eb0d2aa429e81d07866b6
Galaxar <matteo.secco97@gmail.com>
2020-08-24 15:48:52 +0200

    deleted java functions

--- smart_factory/docker_images/bracelets/build.sh
+++
@@ -1,5 +0,0 @@
-mvn clean install
-#replace the tag if you want to modify the fog function
-docker build -t "galaxarum/bracelets" .
-#the modified version must be uploaded to docker hub
-docker push galaxarum/bracelets
