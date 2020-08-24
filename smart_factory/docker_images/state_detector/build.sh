3c46a345ad98a5884db357a50a6da2a344504761
Galaxar <matteo.secco97@gmail.com>
2020-08-24 15:48:52 +0200

    deleted java functions

--- smart_factory/docker_images/state_detector/build.sh
+++
@@ -1,5 +0,0 @@
-mvn clean install
-#replace the tag if you want to modify the fog function
-docker build -t "galaxarum/measure_robot_status_time" .
-#the modified version must be uploaded to docker hub
-docker push galaxarum/measure_robot_status_time
